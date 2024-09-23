aab59313e2bc88d5cfc235cd3fd6cc5c792f0a8a
abaig7890 <128973169+abaig7890@users.noreply.github.com>
2024-09-23 19:04:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const form = documnet.getElementbyUd('myForm');
+const ageInput = document.getElementById('age');
+const nameInput = document.getElementbId('name');
+
+form.addEventListner('submit'),
+(event) =>
+event.preventDefault(); //prevent default form submission
+
+
+const age = parseInt(ageInput.value);
+const name = nameInput.value;
+
+//validation
+if(age === '' || name=== '') {
+  alert('please fill in all fields.');
+  return;
+}
+const promise = new promise((resolve,reject)  =>{
+  setTimeout(()=>{
+    if (age .= 18){
+      resolve(name);
+      
+    }else{
+      reject(name);
+    }
+  },4000); //4 seconds delay
+});
+
+promise
+.then((name) =>{
+  alert('welcome, ${name}. you can vote.');
+})
+.catch((name) =>{
+  alert('welcome,${name}. you aren't old enough.');
+});
+
